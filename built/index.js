2dc05a0e59fe7da2d92a6413d118b9bb1b6e9fdd
candiceguitton <candice.guitton@hotmail.fr>
2019-03-25 12:45:38 +0100

    add typescript to project

---
+++ built/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Pokemon_1 = require("./Pokemon");
+var carapuce = new Pokemon_1.Pokemon('carapuce');
+console.log('le meilleur starter c\'est %s', carapuce.name);
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { Pokemon } from './Pokemon';
+
+const carapuce = new Pokemon('carapuce');
+
+console.log('le meilleur starter c\'est %s', carapuce.name);
